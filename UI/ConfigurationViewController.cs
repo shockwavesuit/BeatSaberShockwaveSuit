9779d3da9eed88f6a0a3feafd9b6d4c834d0fa2d
shockwavesuit <105610349+shockwavesuit@users.noreply.github.com>
2022-07-08 18:25:56 +0100

    Update ConfigurationViewController.cs
    Menu controls for audio to haptic sliders , intensity controls and haptic region controls added.

--- UI/ConfigurationViewController.cs
+++ UI/ConfigurationViewController.cs
@@ -1,4 +1,4 @@
-﻿using BeatSaberMarkupLanguage.MenuButtons;
+using BeatSaberMarkupLanguage.MenuButtons;
 using System;
 using UnityEngine;
 using BeatSaberMarkupLanguage;
@@ -20,6 +20,36 @@ namespace ShockwaveSuit.UI {
                 ModPlugin.SaveModConfiguration();
             }
         }
+        [UIValue("front-torso")]
+        public bool FrontTorsoActivate
+        {
+            get => ModPlugin.cfg.frontTorso;
+            set
+            {
+                ModPlugin.cfg.frontTorso = value;
+                ModPlugin.SaveModConfiguration();
+            }
+        }
+        [UIValue("back-torso")]
+        public bool BackTorsoActivate
+        {
+            get => ModPlugin.cfg.backTorso;
+            set
+            {
+                ModPlugin.cfg.backTorso = value;
+                ModPlugin.SaveModConfiguration();
+            }
+        }
+        [UIValue("legs")]
+        public bool LegsActivate
+        {
+            get => ModPlugin.cfg.legs;
+            set
+            {
+                ModPlugin.cfg.legs = value;
+                ModPlugin.SaveModConfiguration();
+            }
+        }
         [UIValue("saber-pulse-delay")]
         public int SaberPulseRate {
             get => ModPlugin.cfg.saberPulseDelay;
@@ -29,20 +59,66 @@ namespace ShockwaveSuit.UI {
             }
         }
 
+        [UIValue("haptics-intensity")]
+        public float HapticsIntensity
+        {
+            get => ModPlugin.cfg.hapticsIntensity;
+            set
+            {
+                ModPlugin.cfg.hapticsIntensity = value;
+                ModPlugin.SaveModConfiguration();
+            }
+        }
+
+        [UIValue("audio-haptics-intensity")]
+        public float AudioHapticsIntensity { 
+            get => ModPlugin.cfg.audioToHapticsIntensity;
+            set
+            {
+                ModPlugin.cfg.audioToHapticsIntensity = value;
+                ModPlugin.SaveModConfiguration();
+            }
+        }
+
+        [UIValue("bass-ratio")]
+        public float BassRatio
+        {
+            get => ModPlugin.cfg.bassRatio;
+            set
+            {
+                ModPlugin.cfg.bassRatio =1- value +0.09f;
+                ModPlugin.SaveModConfiguration();
+            }
+        }
         [UIValue("selected-haptics-mode")]
         public object SelectedHapticsMode {
             get {
                 return HapticsModeList[(int)ModPlugin.cfg.hapticsMode];
             }
 
-            set {
-                ModPlugin.cfg.hapticsMode = (ModConfiguration.HapticsResponseMode)value;
+            set { 
+               Enum.TryParse(value.ToString(), out ModPlugin.cfg.hapticsMode);
+              ModPlugin.SaveModConfiguration();
+            }
+        }
+        [UIValue("selected-audio-mode")]
+        public object SelectedAudioMode
+        {
+            get
+            {
+                return AudioToHapticsModeList[(int)ModPlugin.cfg.AudioToHapticsMode];
+            }
+
+            set
+            {
+                Enum.TryParse(value.ToString(), out ModPlugin.cfg.AudioToHapticsMode);
                 ModPlugin.SaveModConfiguration();
             }
         }
-
         [UIValue("haptics-mode-list")]
         List<object> HapticsModeList = Enum.GetNames(typeof(ModConfiguration.HapticsResponseMode)).ToList<object>();
+        [UIValue("audio-mode-list")]
+        List<object> AudioToHapticsModeList = Enum.GetNames(typeof(ModConfiguration.AudioToHapticsResponseMode)).ToList<object>();
         // <text id='ban-user-id'></text>
     }
 }
